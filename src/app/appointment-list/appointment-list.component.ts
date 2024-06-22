244bd4d151371e78ae7e97680df67eb9f129de78
Anjali Patidar <anjali.ckpatidar@gmail.com>
2024-06-21 22:48:44 -0400

    Styling

--- src/app/appointment-list/appointment-list.component.ts
+++ src/app/appointment-list/appointment-list.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Appointment } from '../models/appointment';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { OnInit } from '@angular/core'; // Lifecycle hook
 @Component({
   selector: 'app-appointment-list',
   standalone: true,
@@ -9,8 +10,35 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './appointment-list.component.html',
   styleUrls: ['./appointment-list.component.css'] // Corrected property name
 })
-export class AppointmentListComponent {
+export class AppointmentListComponent implements OnInit {
+
   newAppointmentTitle: string = "";
   newAppointmentDate: Date = new Date();
   appointments: Appointment[] = []; 
+
+  ngOnInit(): void { // loading data when browser gets reloaded
+    let savedAppointments = localStorage.getItem("appointments");
+    this.appointments = savedAppointments ? JSON.parse(savedAppointments) : []
+  }
+
+  addAppointment() {
+    if(this.newAppointmentTitle.trim().length && this.newAppointmentDate){
+      let newAppointment: Appointment = {
+        id: Date.now(),
+        title: this.newAppointmentTitle,
+        date: this.newAppointmentDate
+      }
+      this.appointments.push(newAppointment);
+      this.newAppointmentTitle = "";
+      this.newAppointmentDate = new Date();
+
+      localStorage.setItem("appointments", JSON.stringify(this.appointments));
+    }
+  }
+
+  deleteAppointment(index: number){
+    this.appointments.splice(index, 1);
+    localStorage.setItem("appointments", JSON.stringify(this.appointments));
+  }
+
 }
